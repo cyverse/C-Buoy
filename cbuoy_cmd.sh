d7911cc92bbe627e90dad5abeb0ba040adb966ed
jjpalanca <jjpalanca@email.arizona.edu>
2019-05-29 17:33:16 -0700

    startup

--- cbuoy_cmd.sh
+++ cbuoy_cmd.sh
@@ -9,42 +9,28 @@ use_curl(){
     BOOL=0
     PORT=19999
     PORT_ERROR=00000
-    while [ $COUNTER -lt 401 ]; do
-        if [ $COUNTER -eq 0 ]
-        then
-            curl --connect-timeout 5 http://cbuoy.cyverse.org:1247
-            if [ $? -gt 0 ]
-            then
-                BOOL=1
-                PORT_ERROR=1247
-                break
-            fi
+
+    curl --connect-timeout 5 http://cbuoy.cyverse.org:200 >& /dev/null
+    if [ $? -gt 0 ]
+    then
+        BOOL=1
+        PORT_ERROR=1247
+
+    fi
         # COUNTER should be > 0 here
-        else
-            NUM=$(( $PORT + $COUNTER ))
-            curl --connect-timeout 5 http://cbuoy.cyverse.org:$NUM
-            if [ $? -gt 0 ]
-            then
-                BOOL=1
-                PORT_ERROR=$NUM
-                break
-            fi
-        fi
-    done
+
 
     if [ $BOOL -eq 0 ]
     then
         echo "You are connected!"
     else
         echo "ERROR: Port connection error found."
-        IP_ADD=curl ifconfig.co
-        echo "IP Address ($IP_ADD) cannot establish connection with port $PORT_ERROR."
+        IP=$(curl -s ifconfig.co)
+        echo "IP Address ($IP) cannot establish connection with port $PORT_ERROR."
     fi
 }
 
-use_wget(){
 
-}
 
 CMD1=curl
 CMD2=wget
